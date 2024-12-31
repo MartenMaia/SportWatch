817d5d53edd26708b58c43a630c31df32a2b2bc1
Marten Maia <martennmaia@gmail.com>
2024-12-31 10:22:02 -0300

    Commite inicial do projeto

---
+++ Backend/src/main/java/com/sportwatch/demo/WatchSportApplication.java
@@ -0,0 +1,13 @@
+package com.sportwatch.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchSportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchSportApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/sportwatch/demo/WatchSportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportwatch.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchSportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
